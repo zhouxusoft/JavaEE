f003618b3b1742ddf6f5303b451631ac0fb5bfdc
周旭 <zhouxusoft@gmail.com>
2023-09-27 08:50:09 +0800

    new demo

---
+++ teacherdemo/src/main/java/top/godxu/pojo/Class.java
@@ -0,0 +1,27 @@
+package top.godxu.pojo;
+
+public class Class {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Class [id=" + id + ", name=" + name + "]";
+    }
+}
---
+++ teacherdemo/src/main/java/top/godxu/pojo/Teacher.java
@@ -0,0 +1,27 @@
+package top.godxu.pojo;
+
+public class Teacher {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher [id=" + id + ", name=" + name + "]";
+    }
+}
