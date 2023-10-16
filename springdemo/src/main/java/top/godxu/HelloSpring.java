1e349b0ea3e1ba2428ea9ab573a876c909588cc1
周旭 <zhouxusoft@gmail.com>
2023-10-16 10:00:55 +0800

    ..

---
+++ springdemo/src/main/java/top/godxu/HelloSpring.java
@@ -0,0 +1,13 @@
+package top.godxu;
+
+public class HelloSpring {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void show() {
+        System.out.println(name + "HelloSpring");
+    }
+}
--- springdemo/src/main/java/top/godxu/Main.java
+++ springdemo/src/main/java/top/godxu/Main.java
@@ -1,7 +1,12 @@
 package top.godxu;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloSpring helloSpring = (HelloSpring)applicationContext.getBean("helloSpring");
+        helloSpring.show();
     }
 }
